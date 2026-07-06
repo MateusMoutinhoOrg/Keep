52e8e5252938a49832f400e2c26dad3e79b8e4d0
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-07-06 04:42:42 -0300

    feat: initialize database schema in insert sample application

--- samples/insert/insert.go
+++ samples/insert/insert.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	keep_deps "github.com/MateusMoutinhoOrg/Keep/adapters/native"
+	"github.com/MateusMoutinhoOrg/Keep/pkg/database"
 	keep_lib "github.com/MateusMoutinhoOrg/Keep/pkg/keep"
 )
 
 func main() {
 	deps := keep_deps.New()
 	keep := keep_lib.New(deps)
+
+	database := keep.NewDatabase(database.Schema{})
 
 }
