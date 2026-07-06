55bcf942b70439246b9ed54d7f17082b5106bad4
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-07-06 04:10:16 -0300

    feat: implement main function in insert sample with Keep library and native adapters

--- samples/insert/insert.go
+++ samples/insert/insert.go
@@ -1,1 +1,12 @@
-package insert
+package main
+
+import (
+	keep_deps "github.com/MateusMoutinhoOrg/Keep/adapters/native"
+	keep_lib "github.com/MateusMoutinhoOrg/Keep/pkg/keep"
+)
+
+func main() {
+	deps := keep_deps.New()
+	keep := keep_lib.New(deps)
+
+}
