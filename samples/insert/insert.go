d80f1b82e643e84f6d231f568acf530cd4d13f50
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-07-06 04:25:51 -0300

    refactor: remove user insertion logic from insert sample

--- samples/insert/insert.go
+++ samples/insert/insert.go
@@ -9,10 +9,4 @@ func main() {
 	deps := keep_deps.New()
 	keep := keep_lib.New(deps)
 
-	mateus := keep.Insert("user")
-
-	mateus.SetPrimaryKey("email", "mateusmoutinho01@gmail.com")
-	mateus.SetPrimaryKey("username", "mateus")
-	mateus.AddString("password", "12345678")
-
 }
